64a199cc7b4491625caffdb0e3bec4b0d8746ca3
unknown <gouravkashyap42214.com>
2025-09-01 14:32:22 +0530

    changeey

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,8 @@ app.use(cors({
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://devarena-leetcode-frontend.onrender.com"
+    "https://devarena-leetcode-frontend.onrender.com",
+    "https://devarena-leetcode-2.onrender.com"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
